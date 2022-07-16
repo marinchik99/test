6ab80c9454a2314a4c3e9660d336c7709925a88e
marinchik99 <marina_1999@tut.by>
2022-07-16 16:05:14 +0300

    fix: change password validation and add validation to confirm input

--- js/password.js
+++ js/password.js
@@ -1,7 +1,8 @@
-let passwordValue =  document.querySelector(".form-control.password");
+let password =  document.querySelector(".form-control.password");
+let confirmPassword = document.querySelector(".form-control.confirm");
 let pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
 
-passwordValue.addEventListener("input", (e) => {
+function checkValidPassword(e) {
   if (pattern.test(e.target.value)) {
     document.querySelector("#validationServer09").style.display = "none";
     document.querySelector(".form-control.password").style.borderColor = "green";
@@ -10,4 +11,22 @@ passwordValue.addEventListener("input", (e) => {
     document.querySelector("#validationServer09").style.display = "block";
     document.querySelector(".form-control.password").style.borderColor = "red";
   }
+}
+
+function checkConfirmPassword(e) {
+  if (password.value !== e.target.value) {
+    document.querySelector("#validationServer10").style.display = "block";
+    document.querySelector(".form-control.confirm").style.borderColor = "red";
+  }
+  else {
+    document.querySelector("#validationServer10").style.display = "none";
+    document.querySelector(".form-control.confirm").style.borderColor = "green";
+  }
+}
+password.addEventListener("input", (e) => {
+  checkValidPassword(e);
+})
+
+confirmPassword.addEventListener("input", (e) => {
+  checkConfirmPassword(e);
 })
